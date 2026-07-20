8847614e5473417e8fe69646d894371fd6e5c7d9
XDrive Logistics <dannycourierltd@gmail.com>
2026-07-20 20:45:49 +0100

    Use hardened future availability module

--- app/admin/fleet/future-availability/page.tsx
+++ app/admin/fleet/future-availability/page.tsx
@@ -1,1 +1,7 @@
-'use client'; import { FutureAvailabilityPage } from '../../AdminWorkspaceModules'; export default function Page(){return <FutureAvailabilityPage/>}
+'use client';
+
+import { HardenedFutureAvailabilityPage } from '../../AdminWorkspaceHardening';
+
+export default function Page() {
+  return <HardenedFutureAvailabilityPage />;
+}
