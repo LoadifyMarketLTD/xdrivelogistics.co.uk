10ffd762c1393b8192363cb5020e7bc688b50923
XDrive Logistics <dannycourierltd@gmail.com>
2026-08-13 18:46:38 +0100

    Add public enquiries view to broker customers page

--- app/broker/customers/page.tsx
+++ app/broker/customers/page.tsx
@@ -1,1 +1,13 @@
-'use client'; import { BrokerCustomersPage } from '../BrokerWorkspaceModules'; export default function Page(){ return <BrokerCustomersPage/>; }
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { BrokerCustomersPage } from '../BrokerWorkspaceModules';
+import BrokerPublicEnquiries from '../enquiries/BrokerPublicEnquiries';
+
+export default function Page() {
+  const searchParams = useSearchParams();
+  const view = searchParams.get('view');
+
+  if (view === 'enquiries') return <BrokerPublicEnquiries />;
+  return <BrokerCustomersPage />;
+}
