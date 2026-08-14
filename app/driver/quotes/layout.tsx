1ddc7ed2883a108feebed9384d826063cd680194
XDrive Logistics <dannycourierltd@gmail.com>
2026-08-14 09:30:34 +0100

    Apply Loads visual standard to Quotes

--- app/driver/quotes/layout.tsx
+++ app/driver/quotes/layout.tsx
@@ -2,5 +2,5 @@ import type { ReactNode } from 'react';
 import './quotes-exchange.css';
 
 export default function DriverQuotesLayout({ children }: { children: ReactNode }) {
-  return <div className="driver-quotes-exchange">{children}</div>;
+  return <div className="driver-quotes-exchange driver-reference-page driver-reference-quotes">{children}</div>;
 }
