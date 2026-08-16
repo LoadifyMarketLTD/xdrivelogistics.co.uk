6818810b115cf059ac3ea2eba6f9b43086f9aaee
XDrive Logistics <dannycourierltd@gmail.com>
2026-08-16 12:27:20 +0100

    feat(compliance): add company documents route

---
+++ app/admin/documents/company/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import CompanyDocumentsPage from '../../../components/workspace/CompanyDocumentsPage';
+
+export default function CompanyDocumentsRoute() {
+  return <CompanyDocumentsPage />;
+}
