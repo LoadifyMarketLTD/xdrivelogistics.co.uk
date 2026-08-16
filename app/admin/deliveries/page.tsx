cf2185f32f274b04d5597eabc28824c533f4720b
XDrive Logistics <dannycourierltd@gmail.com>
2026-08-16 12:24:56 +0100

    feat(dispatch): add deliveries route

---
+++ app/admin/deliveries/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import DispatchExecutionQueuePage from '../../components/workspace/DispatchExecutionQueuePage';
+
+export default function DeliveriesPage() {
+  return <DispatchExecutionQueuePage mode="deliveries" />;
+}
