b05910d9a305d34386307983d189c31373bb6d6d
XDrive Logistics <dannycourierltd@gmail.com>
2026-08-16 12:24:43 +0100

    feat(dispatch): add collections route

---
+++ app/admin/collections/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import DispatchExecutionQueuePage from '../../components/workspace/DispatchExecutionQueuePage';
+
+export default function CollectionsPage() {
+  return <DispatchExecutionQueuePage mode="collections" />;
+}
