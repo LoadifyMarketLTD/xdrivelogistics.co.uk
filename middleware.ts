86d1cb9df956a8d6c35211f18b0a80ab679b4c2b
XDrive Logistics <dannycourierltd@gmail.com>
2026-08-18 22:50:40 +0100

    fix(runtime): run middleware on Node.js

--- middleware.ts
+++ middleware.ts
@@ -718,6 +718,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
+  runtime: 'nodejs',
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico|manifest.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|map)$).*)',
   ],
