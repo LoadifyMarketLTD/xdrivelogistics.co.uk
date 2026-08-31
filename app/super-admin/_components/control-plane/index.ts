5c28aada49370e554f640a395b5901bfad276d2e
XDrive Logistics <dannycourierltd@gmail.com>
2026-08-31 01:02:16 +0100

    Export Super Admin control-plane primitives

---
+++ app/super-admin/_components/control-plane/index.ts
@@ -0,0 +1,15 @@
+export { default as PlatformActionPanel } from './PlatformActionPanel';
+export { default as PlatformAuditTimeline } from './PlatformAuditTimeline';
+export { default as PlatformCaseCentre } from './PlatformCaseCentre';
+export { default as PlatformEntityInspector } from './PlatformEntityInspector';
+export { default as PlatformEntityLink, platformEntityHref } from './PlatformEntityLink';
+export type {
+  PlatformAuditEntry,
+  PlatformCaseSeverity,
+  PlatformCaseStatus,
+  PlatformCaseSummary,
+  PlatformEntityField,
+  PlatformEntitySection,
+  PlatformEntityType,
+  PlatformSemanticAction,
+} from './types';
