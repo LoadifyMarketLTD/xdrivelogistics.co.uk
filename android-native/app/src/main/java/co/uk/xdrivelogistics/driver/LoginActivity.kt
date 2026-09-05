0ee21a7d051058ba972c5b4f6bb343e3514ab00a
XDrive Logistics <dannycourierltd@gmail.com>
2026-09-05 01:09:07 +0100

    [skip actions] Fix Kotlin 2.1 RowScope weight resolution

--- android-native/app/src/main/java/co/uk/xdrivelogistics/driver/LoginActivity.kt
+++ android-native/app/src/main/java/co/uk/xdrivelogistics/driver/LoginActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
