cf510a33104f684071559bef44631e32720a6d8a
XDrive Logistics <dannycourierltd@gmail.com>
2026-09-06 10:10:00 +0100

    Add Driver Preview native location package

---
+++ apps/xdrive-driver-phone-golden/android/app/src/main/java/co/uk/xdrivelogistics/driver/XDriveLocationPackage.kt
@@ -0,0 +1,14 @@
+package co.uk.xdrivelogistics.driver
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class XDriveLocationPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
+    listOf(XDriveLocationModule(reactContext))
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> =
+    emptyList()
+}
