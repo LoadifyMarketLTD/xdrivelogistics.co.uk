d707986df4b65aadc85bd18598d730c0acd17dc6
XDrive Logistics <dannycourierltd@gmail.com>
2026-09-06 10:09:54 +0100

    Add native location module package to Driver Preview

--- apps/xdrive-driver-phone-golden/android/app/src/main/java/co/uk/xdrivelogistics/driver/MainApplication.kt
+++ apps/xdrive-driver-phone-golden/android/app/src/main/java/co/uk/xdrivelogistics/driver/MainApplication.kt
@@ -23,8 +23,7 @@ class MainApplication : Application(), ReactApplication {
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
             val packages = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(MyReactNativePackage())
+            packages.add(XDriveLocationPackage())
             return packages
           }
 
@@ -44,7 +43,6 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
